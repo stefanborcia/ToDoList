0d78a53d9b9ae4d649df55eea6d03714218c2436
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-03-07 17:51:57 +0100

    added Update/Edit button to update or edit the data from our tabel

--- ToDoList/Form1.cs
+++ ToDoList/Form1.cs
@@ -54,5 +54,34 @@ namespace ToDoList
             con.Close();
             bind_Data();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd3 = new SqlCommand(
+                    "Update Table1 Set day=@day,time=@time,todo=@todo,requirements=@requirements where id=@id",
+                    con);
+            cmd3.Parameters.AddWithValue("day", DateTime.Parse(textBox2.Text));
+            cmd3.Parameters.AddWithValue("time", DateTime.Parse(textBox3.Text));
+            cmd3.Parameters.AddWithValue("todo", textBox4.Text);
+            cmd3.Parameters.AddWithValue("requirements", textBox5.Text);
+            cmd3.Parameters.AddWithValue("id", textBox1.Text);
+
+            con.Open();
+            cmd3.ExecuteNonQuery();
+            con.Close();
+            bind_Data();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index;
+            index = e.RowIndex;
+            DataGridViewRow selectedrow = dataGridView1.Rows[index];
+            textBox1.Text = selectedrow.Cells[0].Value.ToString();
+            textBox2.Text = selectedrow.Cells[1].Value.ToString();
+            textBox3.Text = selectedrow.Cells[2].Value.ToString();
+            textBox4.Text = selectedrow.Cells[3].Value.ToString();
+            textBox5.Text = selectedrow.Cells[4].Value.ToString();
+        }
     }
 }
