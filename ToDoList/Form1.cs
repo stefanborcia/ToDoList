a0b4aedf23a9b1df14ab78cf9724921abc0965fb
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-03-07 13:57:21 +0100

    Add project files.

---
+++ ToDoList/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ToDoList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection("Data source=.; initial catalog=ToDoListDB;integrated security=true");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bind_Data();
+        }
+
+        private void bind_Data()
+        {
+            SqlCommand cmd1 =
+                new SqlCommand(
+                    "Select id As Number,day As Day,time As Time,todo As ToDo,requirements As Requirements from Table1", con);
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd1;
+            DataTable dt = new DataTable();
+            dt.Clear();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
